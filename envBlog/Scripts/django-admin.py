da8db8aac3def630ab8247e233051ec16899aa40
Lina Marcela Patiño Pineda <linampatino@gmail.com>
2018-07-15 15:19:21 -0500

    Changed the html for post_list

---
+++ envBlog/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!F:\Portafolio\django-girls\My-first-project-django\envBlog\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
